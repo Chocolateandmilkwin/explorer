efaf27c07299cf71de402bdf213928d72584ef13
Nicklas Nielsen <nicknielsen1995@hotmail.com>
2024-11-23 20:30:34 +0100

    First version Fixes #1

--- ui/lib/main.dart
+++ ui/lib/main.dart
@@ -1,10 +1,6 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:reorderable_tabbar/reorderable_tabbar.dart';
-
-File file = File('lib/main.dart');
 
 void main() {
   runApp(Home());
@@ -36,39 +32,78 @@ class Home extends StatelessWidget {
     return MaterialApp(
         home: Obx(
       () => DefaultTabController(
+          animationDuration: Duration(milliseconds: 1),
           length: controller.tabs.length,
           child: Scaffold(
-            appBar: ReorderableTabBar(
-              isScrollable: true,
-              buildDefaultDragHandles: false,
-              tabs: controller.tabs
-                  .map((e) => Tab(
-                        child: SizedBox(
-                          width: 100,
-                          child: Row(
-                            children: [
-                              Text(e.title),
-                              IconButton(
-                                  icon: const Icon(Icons.close),
-                                  onPressed: () {
-                                    controller.tabs.remove(e);
-                                  })
-                            ],
-                          ),
-                        ),
-                      ))
-                  .toList(),
-              onReorder: (p0, p1) => controller.reorder(p0, p1),
-            ),
+            appBar: PreferredSize(
+                preferredSize: const Size.fromHeight(142),
+                child: Column(
+                  children: [
+                    ReorderableTabBar(
+                      isScrollable: true,
+                      buildDefaultDragHandles: false,
+                      padding: const EdgeInsets.all(0),
+                      tabs: controller.tabs
+                          .map((e) => Tab(
+                                height: 50,
+                                child: Container(
+                                  color: Color(0xffe0e0e0),
+                                  width: 200,
+                                  height: 50,
+                                  child: Row(
+                                    children: [
+                                      Text(e.title),
+                                      IconButton(
+                                          icon: const Icon(Icons.close),
+                                          onPressed: () {
+                                            controller.tabs.remove(e);
+                                          })
+                                    ],
+                                  ),
+                                ),
+                              ))
+                          .toList(),
+                      onReorder: (p0, p1) => controller.reorder(p0, p1),
+                    ),
+                    Container(
+                        height: 45,
+                        color: const Color.fromARGB(255, 255, 0, 221),
+                        child: const Row(
+                          children: [Text('title')],
+                        )),
+                    Container(
+                        height: 45,
+                        color: const Color(0xffe0e0e0),
+                        child: const Row(
+                          children: [Text('title')],
+                        )),
+                  ],
+                )),
+
+            // AppBar(
+            //   toolbarHeight: 200,
+            //   flexibleSpace: Column(
+            //     children: [
+            //
+
+            //     ],
+            //   ),
+            // ),
             body: TabBarView(
                 children: controller.tabs
                     .map((e) => Browser(title: e.title))
                     .toList()),
+            bottomNavigationBar: Container(
+                height: 20,
+                color: const Color(0xffe0e0e0),
+                child: const Row(
+                  children: [Text('title')],
+                )),
             floatingActionButton: FloatingActionButton(
               onPressed: () {
                 controller.addTab();
               },
-              child: Icon(Icons.add),
+              child: const Icon(Icons.add),
             ),
           )),
     ));
@@ -80,40 +115,20 @@ class Browser extends StatelessWidget {
   final String title;
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Row(
       children: [
         Container(
-            height: 40,
-            color: Color.fromARGB(255, 255, 0, 221),
-            child: Row(
-              children: [Text(title)],
-            )),
-        Container(
-            height: 40,
-            color: Color(0xffe0e0e0),
-            child: Row(
-              children: [Text(title)],
-            )),
+          width: 200,
+          color: const Color.fromARGB(255, 206, 46, 46),
+          child: SizedBox.expand(child: Text(title)),
+        ),
         Expanded(
-            child: Row(
-          children: [
-            Container(
-              width: 200,
-              color: Color.fromARGB(255, 206, 46, 46),
-              child: Text(title),
-            ),
-            Expanded(
-              child: Container(
-                  color: Color.fromARGB(255, 20, 130, 46), child: Text(title)),
-            )
-          ],
-        )),
-        Container(
-            height: 20,
-            color: Color(0xffe0e0e0),
-            child: Row(
-              children: [Text(title)],
-            )),
+          child: SizedBox.expand(
+            child: Container(
+                color: const Color.fromARGB(255, 20, 130, 46),
+                child: Text(title)),
+          ),
+        )
       ],
     );
   }
